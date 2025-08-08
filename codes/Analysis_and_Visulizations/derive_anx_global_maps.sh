a641f9954ba5ad915bad62b0337a02cf5eed0db5
k.kundert-obando <73260292+krogge-obando@users.noreply.github.com>
2025-08-08 11:11:12 -0500

    Update derive_anx_global_maps.sh

--- codes/Analysis_and_Visulizations/derive_anx_global_maps.sh
+++ codes/Analysis_and_Visulizations/derive_anx_global_maps.sh
@@ -1,3 +1,4 @@
 # sh code we used to derive the brain maps associated with state anxiety. Please refer to code derive4Dmaps.m to see how we made our arousal_map_543_subj.nii file.
+#For information on how to make a contrast matrix or design matrix go here : https://web.mit.edu/fsl_v5.0.10/fsl/doc/wiki/GLM(2f)CreatingDesignMatricesByHand.html
 
 randomise -i /data1/neurdylab/datasets/nki_rockland/vigilance_analysis/arousal_map_543_subj.nii  -o /data1/neurdylab/datasets/nki_rockland/vigilance_analysis/fslrandomise_5000/state_543_arousal_randomise_output -d state_model.mat -t contrast.con -m /data1/neurdylab/MNI152_T1_2mm_brain_mask_filled.nii.gz -n 5000 -T -D  
